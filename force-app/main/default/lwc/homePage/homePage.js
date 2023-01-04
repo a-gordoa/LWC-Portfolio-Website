0099d36863333b735a804449bc9ca27cf7fa1cf4
Anthony Gordoa <johngordoa@gmail.com>
2023-01-04 12:05:57 -0600

    first commit

---
+++ force-app/main/default/lwc/homePage/homePage.js
@@ -0,0 +1,42 @@
+import { LightningElement } from 'lwc';
+import MEYAWO from '@salesforce/resourceUrl/meyawo';
+import HEADSHOT from '@salesforce/resourceUrl/Anthony';
+import LAMPS from '@salesforce/resourceUrl/lamps';
+import {loadScipt,loadStyle} from 'lightning/platformResourceLoader';
+
+export default class HomePage extends LightningElement {
+
+    lamps = LAMPS;
+    
+    headshot = HEADSHOT;
+
+    isLibLoaded = false;
+
+   
+    renderedCallback(){
+        if (this.isLibLoaded) {
+            return;
+        } else {
+            Promise.all([
+                // loadStyle(this,MEYAWO+'/meyawo/meyawo/css/meyawo.min.css')
+                loadStyle(this,MEYAWO)
+            ])
+            .then({
+                
+            })
+            .catch(error=>{
+                console.log('error = ' + error);
+            })
+            this.isLibLoaded = true;
+            console.log('Loaded2');
+            console.log(this.lamps);    
+        }
+    }
+
+    get lampHeader(){
+        return `background-color:blue;background-image:url(${this.lamps})`;
+    }
+
+
+
+}
---
+++ force-app/main/default/lwc/projectTiles/projectTiles.js
@@ -0,0 +1,33 @@
+import { LightningElement } from 'lwc';
+import SPOTIFYLOGO from '@salesforce/resourceUrl/SpotifyLogo'
+import TO_DO_IMAGE from '@salesforce/resourceUrl/ToDoImage'
+
+export default class ProjectTiles extends LightningElement {
+//spotifyLogo = SPOTIFYLOGO;
+
+projectList = [
+    {
+        name:'Spotify Playlist App',
+        description:'This component uses the Spotify API to allow users to create and edit Spotfy Playlists directly within the component',
+        image: `${SPOTIFYLOGO}`,
+        githubLink: 'https://github.com/a-gordoa/Spotify-LWC-Project',
+        techList: ['@wire', 'javascript',]
+
+    },
+    {
+        name:'To Do List',
+        description:'This is a description for the To Do app that I haven\'t build',
+        image: `${TO_DO_IMAGE}`,
+        githubLink: 'https://github.com/a-gordoa/Spotify-LWC-Project',
+        techList: ['@wire', ]
+    },
+    {
+        name:'One last project',
+        description:'This is a description for the To Do app that I haven\'t build',
+        image: `${SPOTIFYLOGO}`,
+        githubLink: 'https://github.com/a-gordoa/Spotify-LWC-Project',
+        techList: ['@wire', ]
+    }
+]
+
+}
